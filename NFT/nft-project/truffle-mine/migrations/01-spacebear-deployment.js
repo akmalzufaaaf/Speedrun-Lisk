53ec8b809c46deb6f15eb9774cc85cf8e1e08657
akmalzufaaaf <akmalzufafathurrrahman@ugm.ac.id>
2025-10-12 22:11:54 +0700

    Fix: Correct repository root to Speedrun-Lisk folder

--- NFT/nft-project/truffle-mine/migrations/01-spacebear-deployment.js
+++
@@ -1,5 +0,0 @@
-const Spacebears = artifacts.require("SpacebearDebug");
- 
-module.exports = function(deployer, network, account) {
-    deployer.deploy(Spacebears, account[0]);
-}
--- NFT/nft-project/truffle/migrations/01-spacebears-deployment.js
+++
@@ -1,5 +0,0 @@
-const Spacebears = artifacts.require("SpaceBear");
-
-module.exports = function(deployer, network, accounts) {
-    deployer.deploy(Spacebears, accounts[0]);
-}
