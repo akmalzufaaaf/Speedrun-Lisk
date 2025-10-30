d6dba7e39cc2adacce8e1e447e9ab1602490d977
akmalzufaaaf <akmalzufafathurrahman@mail.ugm.ac.id>
2025-10-30 20:01:38 +0700

    week-5 progress: checkpoint 1

---
+++ packages/hardhat/deploy/02_deploy_marketplace.ts
@@ -0,0 +1,19 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const deployNFTMarketplace: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployer } = await hre.getNamedAccounts();
+    const { deploy, get } = await hre.deployments;
+  
+    const myNFT = await get("MyNFT");
+    
+    await deploy("NFTMarketplace", {
+        from: deployer,
+        args: [myNFT.address],
+        log: true,
+        autoMine: true,
+    });
+};
+
+export default deployNFTMarketplace;
+deployNFTMarketplace.tags = ["NFTMarketplace"];
